ede117e7797f8bc7deeebf08db3a584c1e25dab8
Xiaoyu Tai <taixiaoyu@gmail.com>
2016-08-19 15:15:45 -0400

    Add mirador versions and customized manifest.

--- mirador/site.js
+++ mirador/site.js
@@ -1,7 +1,8 @@
-var manifests = "http://iiif-sandbox.lib.umd.edu/manifests/sn83045081/1902-01-15/issue.json";
+var manifests = "./manifest.json";
+var annotation = "./annotation.json";
 jQuery(function() {
   if (typeof manifests === undefined || manifests === null || manifests === "") {
-    var manifests = "http://iiif-sandbox.lib.umd.edu/manifests/sn83045081/1902-01-15/issue.json";
+    var manifests = "./manifest.json";
   }
   window.console.log(encodeURIComponent(manifests));
 });
@@ -15,13 +16,14 @@ $(function() {
     "data": [
       { "manifestUri": manifests, "location": "University of Maryland" }
     ],
-    "windowObjects": [{
-      "loadedManifest": manifests,
-      "viewType": "ImageView",
-      "displayLayout": false,
-      "bottomPanel": true,
-      "sidePanel": false,
-      "annotationLayer": false
-    }]
+    "windowObjects": [
+      {
+        "loadedManifest": manifests,
+        "viewType": "ImageView",
+        "displayLayout": false,
+        "sidePanel": false,
+        "annotationState": "annoOnCreateOff"
+      }
+    ]
   });
 });
